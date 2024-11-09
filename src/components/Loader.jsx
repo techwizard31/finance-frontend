c9a65f34ce7af378f11d261281049f4ef26066e8
techwizard31 <istaprasad.patra@gmail.com>
2024-11-10 01:54:31 +0530

    done and dusted

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import "./Loader.css";
 
 function Loader() {
   return (
-    <div className="w-full h-screen absolute z-11 flex justify-center items-center">
+    <div className="w-full h-screen absolute z-100 flex justify-center items-center">
       <div className="dot-spinner">
         <div className="dot-spinner__dot"></div>
         <div className="dot-spinner__dot"></div>
